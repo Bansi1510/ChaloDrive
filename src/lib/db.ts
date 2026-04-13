418fb826b5bccd241ad19f49ebdde505779c5cc2
prajapati bansi <prajapatibansi1510@gmail.com>
2026-04-13 13:42:35 +0530

    setup done

---
+++ src/lib/db.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const databaseUrl = process.env.MONGODB;
+
+if (!databaseUrl) {
+  throw new Error("Database Url Not Founded");
+}
+
+let catched = global.mongoDbConnection;
+
+if (!catched) {
+  catched = global.mongoDbConnection = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (catched.conn) {
+    return catched.conn;
+  }
+  if (!catched.promise) {
+    catched.promise = mongoose.connect(databaseUrl).then(c => c.connection);
+  }
+
+  try {
+    const conn = await catched.promise;
+    console.log("Database connected sucessfully")
+    return conn;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default connectDB;
